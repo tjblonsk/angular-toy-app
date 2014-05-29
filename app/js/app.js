543ba7630d8142e35346e1456295e3cc50495e17
Tim <tim.blonski@gmail.com>
2014-05-29 19:35:21 -0400

    environment configured

---
+++ app/js/app.js
@@ -0,0 +1,6 @@
+(function() {
+  "use strict";
+
+  var fundbase = angular.module('Fundbase', []);
+
+})();
---
+++ app/js/controllers.js
@@ -0,0 +1,9 @@
+(function() {
+  "use strict";
+
+  angular.module('Fundbase').controller('PizzasController', function($scope) {
+    $scope.pizza = {name: 'Pepperoni'}
+  });
+
+
+})();
