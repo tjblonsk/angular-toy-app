3c5ff523ccc95debe0174cb47acc73d8ee52bb84
Tim <tim.blonski@gmail.com>
2014-06-01 15:12:50 -0400

    configuring app and specs

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,7 @@
-(function() {
-  "use strict";
-
-  var fundbase = angular.module('Fundbase', []);
+"use strict";
 
   angular.module('Fundbase', [
-    // 'ngRoute',
-    // 'Fundbase.services',
-    // 'Fundbase.directives',
+    'Fundbase.services',
+    'Fundbase.directives',
     'Fundbase.controllers'
-  ]).
-  config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
-    $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
-    $routeProvider.otherwise({redirectTo: '/view1'});
-  }]);
-
-})();
+  ]);
