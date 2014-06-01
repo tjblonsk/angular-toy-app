28156730769bb7e72fbf7a53db25d92b44829880
Tim <tim.blonski@gmail.com>
2014-06-01 15:16:45 -0400

    controllers implemented for PizzasController and OrderController.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,11 +1,15 @@
-(function() {
-  "use strict";
+"use strict";
 
   angular.module('Fundbase.controllers', [])
-    .controller('PizzasController', ['$scope', function($scope) {
-      $scope.pizza = {name: 'Pepperoni'}
+    .controller('PizzasController', ['$scope', 'pizzasRepo', function($scope, pizzasRepo) {
+      $scope.pizzasRepo = pizzasRepo;
+      $scope.pizzas = $scope.pizzasRepo.getPizzas().success(function(resp) {
+        $scope.pizzas = resp;
+      });;
+
+    }])
+    .controller('OrderController', ['$scope', 'orderRepo', function($scope, orderRepo) {
+      $scope.orderRepo = orderRepo;
+      $scope.items = $scope.orderRepo.getItems();
 
     }]);
-
-
-})();
